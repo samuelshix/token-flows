a1c9667b74632e592d8ee4bdf7df71b12155285c
samuelshix <samuel.li.shi@gmail.com>
2022-07-12 13:39:34 -0400

    a

--- token_flows/settings.py
+++ token_flows/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
     'app'
